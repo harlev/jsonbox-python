3caefedb5a276148adc33c7d949c1a5a3d4fb575
rharlev <ron.harlev@genesys.com>
2019-09-16 16:16:54 -0700

    basic version

---
+++ jsonbin.py
@@ -0,0 +1,60 @@
+import json
+import requests
+
+
+
+class JsonBin:
+    RECORD_ID_KEY = "_id"
+
+    def __init__(self, service_host="https://jsonbox.io"):
+        self.service_host = service_host
+
+    def _get_url(self, box_id, collection_or_record):
+        if collection_or_record:
+            url = "{0}/{1}/{2}".format(self.service_host, box_id, collection_or_record)
+        else:
+            url = "{0}/{1}".format(self.service_host, box_id)
+        return url
+
+    def get_record_id(self, data):
+        return data[self.RECORD_ID_KEY]
+
+    def read(self, box_id, collection_or_record=None):
+        url = self._get_url(box_id, collection_or_record)
+
+        response = requests.get(url)
+        if response.ok:
+            json_data = response.json()
+            return json_data
+        else:
+            return False
+
+    def write(self, data, box_id, collection=None):
+        url = self._get_url(box_id, collection)
+
+        response = requests.post(url, json=data)
+        if response.ok:
+            json_data = response.json()
+            return json_data
+        else:
+            return False
+
+    def update(self, data, box_id, record_id):
+        url = self._get_url(box_id, record_id)
+
+        response = requests.put(url, json=data)
+        if response.ok:
+            json_data = response.json()
+            return json_data
+        else:
+            return False
+
+    def delete(self, box_id, record_id):
+        url = self._get_url(box_id, record_id)
+
+        response = requests.delete(url)
+        if response.ok:
+            json_data = response.json()
+            return json_data
+        else:
+            return False
---
+++ tests/test_jsonbin.py
@@ -0,0 +1,95 @@
+import unittest
+from jsonbin import JsonBin
+
+TEST_BOX_ID = "test_gywvcu8ew7t7gascbascbuwd"
+TEST_COLLECTION_ID = "collection_427453"
+TEST_RECORD_ID = "test_sjdgfygsf2347623564twfgyu"
+TEST_DATA_KEY_1 = "aaa"
+TEST_DATA_VALUE_1 = "bbb"
+TEST_DATA_KEY_2 = "ccc"
+TEST_DATA_VALUE_2 = "ddd"
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.jb = JsonBin()
+
+    def test_read_record(self):
+        json_data = self.jb.read(TEST_BOX_ID, "5d80031fca4f06001791fb28")
+        self.assertIsNotNone(json_data)
+        self.assertFalse(isinstance(json_data, list))
+        print(json_data)
+
+    def test_read_box(self):
+        json_data = self.jb.read(TEST_BOX_ID)
+        self.assertIsNotNone(json_data)
+        self.assertTrue(isinstance(json_data, list))
+
+        print(json_data)
+
+    def test_write_box(self):
+        data = {
+            TEST_DATA_KEY_1: TEST_DATA_VALUE_1
+        }
+        json_data = self.jb.write(data, TEST_BOX_ID)
+        self.assertIsNotNone(json_data)
+        self.assertEqual(json_data[TEST_DATA_KEY_1], TEST_DATA_VALUE_1)
+
+        print(json_data)
+
+    def test_write_box_collection(self):
+        data = {
+            TEST_DATA_KEY_1: TEST_DATA_VALUE_1
+        }
+        json_data = self.jb.write(data, TEST_BOX_ID, TEST_COLLECTION_ID)
+        self.assertIsNotNone(json_data)
+        self.assertEqual(json_data[TEST_DATA_KEY_1], TEST_DATA_VALUE_1)
+
+        print(json_data)
+
+    def test_update(self):
+        data = {
+            TEST_DATA_KEY_1: TEST_DATA_VALUE_1
+        }
+        json_data = self.jb.write(data, TEST_BOX_ID)
+        self.assertIsNotNone(json_data)
+        self.assertEqual(json_data[TEST_DATA_KEY_1], TEST_DATA_VALUE_1)
+
+        record_id = self.jb.get_record_id(json_data)
+
+        json_data = self.jb.read(TEST_BOX_ID, record_id)
+        self.assertIsNotNone(json_data)
+        self.assertEqual(json_data[TEST_DATA_KEY_1], TEST_DATA_VALUE_1)
+
+        data = {
+            TEST_DATA_KEY_2: TEST_DATA_VALUE_2
+        }
+        self.jb.update(data, TEST_BOX_ID, record_id)
+
+        json_data = self.jb.read(TEST_BOX_ID, record_id)
+        self.assertIsNotNone(json_data)
+        self.assertEqual(json_data[TEST_DATA_KEY_2], TEST_DATA_VALUE_2)
+
+        print(json_data)
+
+    def test_delete(self):
+        data = {
+            TEST_DATA_KEY_1: TEST_DATA_VALUE_1
+        }
+        json_data = self.jb.write(data, TEST_BOX_ID)
+        self.assertIsNotNone(json_data)
+        self.assertEqual(json_data[TEST_DATA_KEY_1], TEST_DATA_VALUE_1)
+
+        record_id = self.jb.get_record_id(json_data)
+
+        json_data = self.jb.delete(TEST_BOX_ID, record_id)
+        self.assertIsNotNone(json_data)
+
+        reuslt = self.jb.read(TEST_BOX_ID, record_id)
+        self.assertFalse(reuslt)
+
+        print(json_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
