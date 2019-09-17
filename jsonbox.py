f507232655823ceb44b45f7b9350870abf3a55e9
rharlev <ron.harlev@genesys.com>
2019-09-17 10:11:08 -0700

    add sort support

--- jsonbox.py
+++ jsonbox.py
@@ -8,18 +8,25 @@ class JsonBox:
     def __init__(self, service_host="https://jsonbox.io"):
         self.service_host = service_host
 
-    def _get_url(self, box_id, collection_or_record):
+    def _get_url(self, box_id, collection_or_record=None, sort_by=None):
+        url = "{0}/{1}".format(self.service_host, box_id)
+
         if collection_or_record:
-            url = "{0}/{1}/{2}".format(self.service_host, box_id, collection_or_record)
-        else:
-            url = "{0}/{1}".format(self.service_host, box_id)
+            url = "{0}/{1}".format(url, collection_or_record)
+
+        if sort_by:
+            url = "{0}?sort={1}".format(url, sort_by)
+
         return url
 
     def get_record_id(self, data):
-        return data[self.RECORD_ID_KEY]
+        if isinstance(data, list):
+            return [item[self.RECORD_ID_KEY] for item in data]
+        else:
+            return data[self.RECORD_ID_KEY]
 
-    def read(self, box_id, collection_or_record=None):
-        url = self._get_url(box_id, collection_or_record)
+    def read(self, box_id, collection_or_record=None, sort_by=None):
+        url = self._get_url(box_id, collection_or_record, sort_by)
 
         response = requests.get(url)
         if response.ok:
