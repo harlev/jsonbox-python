e9fcaf29e442a40ce2281bc1be9156d4c755f07d
rharlev <ron.harlev@genesys.com>
2019-09-16 18:51:16 -0700

    update supported python version

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ setup(
     name='jsonbox',
     description='Python wrapper for jsonbox.io',
     url='https://github.com/harlev/jsonbox-python',
-    version='0.1.3',
+    version='0.1.5',
     packages=['.'],
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=['requests'],
-    python_requires='==2.7.*',
+    python_requires='>=2.7',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
 )
