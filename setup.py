a41914889870f2e4b26fdbd4aaffc70ed49c7bce
rharlev <ron.harlev@genesys.com>
2019-09-16 17:22:09 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='jsonbox',
+    description='Python wrapper for jsonbox.io',
+    url='https://github.com/harlev/jsonbox-python',
+    version='0.1.0',
+    packages=['jsonbox'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=['requests'],
+    python_requires='==2.7.*',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
