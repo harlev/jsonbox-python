0bf739fe0c3cf8de3fced5814b0f6f3bf55b9e57
Ron Harlev <harlev@gmail.com>
2019-09-18 12:38:13 -0700

    update examples

--- examples/example.py
+++ examples/example.py
@@ -7,7 +7,7 @@ MY_BOX_ID = str(uuid.uuid4()).replace("-", "_")
 # create instance
 jb = JsonBox()
 
-data = [{"name": "David", "age": "25"}, {"name": "Alice", "age": "19"}]
+data = [{"name": "first", "age": 25}, {"name": "second", "age": 19}]
 
 # write data
 result = jb.write(data, MY_BOX_ID)
@@ -23,6 +23,16 @@ print(jb.read(MY_BOX_ID))
 
 # read all records in box with sort
 print(jb.read(MY_BOX_ID, sort_by="age"))
+
+# read records in box with sort matching query (see documentation for syntax)
+print(jb.read(MY_BOX_ID, query="name:firs*"))
+print(jb.read(MY_BOX_ID, query="age:=19"))
+
+# read records with limit
+print(jb.read(MY_BOX_ID, limit=1))
+
+# read records with skip
+print(jb.read(MY_BOX_ID, skip=1))
 
 # update data
 data = {"name": "Bob", "age": "25"}
